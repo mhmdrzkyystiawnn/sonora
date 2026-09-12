529588b5eb43324e9557bcda3046cb711aade947
mhmdrzkyystiawnn <gamaugelay524@gmail.com>
2026-09-12 20:34:49 +0700

    fix: update CORS to sonora.muhamadrizky.my.id

--- sonora-api/src/app.ts
+++ sonora-api/src/app.ts
@@ -1,13 +1,16 @@
 /// <reference lib="cloudflare" />
 
 import { Hono } from "hono";
+import { env } from "./lib/env";
 
 const app = new Hono();
 
 app.use("*", async (c, next) => {
-  c.res.headers.set("Access-Control-Allow-Origin", "http://localhost:5173");
+  const origin = env.CORS_ORIGIN ?? "https://sonora.muhamadrizky.my.id";
+  c.res.headers.set("Access-Control-Allow-Origin", origin);
   c.res.headers.set("Access-Control-Allow-Credentials", "true");
   c.res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  c.res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (c.req.method === "OPTIONS") return c.text("", 204);
   await next();
 });
